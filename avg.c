fe47e52fb9cb5a7cf1db5e4ff151939fc970f8ee
BibinBinu <117814669+bibin123binu@users.noreply.github.com>
2022-11-09 15:14:00 +0530

    Add files via upload

---
+++ avg.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+void main(){
+int a,b,c,avg=0;
+printf("enter a,b,c");
+scanf("%d%d%d",&a,&b,&c);
+avg=(a+b+c)/3;
+printf("avg=%d",avg);
+}
---
+++ sum.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+void main(){
+int a,b,sum=0;
+printf("enter a and b ");
+scanf("%d%d",&a,&b);
+sum=a+b;
+printf("the sum =%d",sum);
+}
+
